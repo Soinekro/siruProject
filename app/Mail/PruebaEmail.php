0995d10f07863dbcca7ce14f752e06c19e18d750
deveiap <85376092+deveiap@users.noreply.github.com>
2022-10-07 12:31:28 -0500

    Clase email

---
+++ app/Mail/PruebaEmail.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class PruebaEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance. Prueba
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('view.name');
+    }
+    public function espacio(){
+        return('welcome.blade');//boorar esta funcion
+    }
+}
